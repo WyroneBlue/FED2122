156a46b8dce6401f54f72a4dfbe7479a3991b798
Ymaro <wyroneblue@gmail.com>
2021-09-25 00:30:29 +0200

    added sort by desktop

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ var title = document.querySelector('#shoe-title');
 var filterOpenButton = document.querySelector('section#shoe-title > section > button:first-of-type');
 var filterOpenButtonSpan = document.querySelector('section#shoe-title > section > button:first-of-type span');
 var filterClosed = false;
+
+var sortByOpenButton = document.querySelector('section#shoe-title > section > button:last-of-type');
+var sortByOpenButtonSpan = document.querySelector('section#shoe-title > section > button:last-of-type span');
+var sortByOpenChevron = document.querySelector('section#shoe-title > section > button:last-of-type i');
+var sortByOptions = document.querySelectorAll('section#shoe-title > section > div span');
+var sortByOpen = false;
 var hamburger = document.querySelector('#hamburger > img:last-of-type');
 var filterOpenButton = document.querySelector('#filter-buttons > button');
 var filterCloseButton = document.querySelector('#filter-options > section:first-of-type > button');
@@ -17,6 +23,28 @@ var body = document.body;
 var blurSection = document.querySelector('body > section');
 var menuOpen = false;
 
+const toggleSortByMenu = function(){
+    body.classList.toggle('sortby-open');
+    sortByOpen = !sortByOpen;
+    if(sortByOpen){
+        sortByOpenChevron.classList = 'fas fa-chevron-down';
+    } else {
+        sortByOpenChevron.classList = 'fas fa-chevron-up';
+    }
+}
+
+const setSortBy = function(e){
+    resetSortBy();
+    e.target.classList.add('active');
+    let sort = e.target.textContent;
+    sortByOpenButtonSpan.textContent = `: ${sort}`
+}
+
+const resetSortBy = function(){
+    sortByOptions.forEach(option => {
+        option.classList.remove('active');
+    });
+}
 const toggleMobileMenu = function(){
     body.classList.toggle('menu-open');
     menuOpen = !menuOpen;
@@ -66,6 +94,10 @@ hamburger.addEventListener('click', toggleMobileMenu);
 blurSection.addEventListener('click', toggleMobileMenu);
 filterOpenButton.addEventListener('click', toggleFilterMenu);
 filterCloseButton.addEventListener('click', toggleFilterMenu);
+sortByOpenButton.addEventListener('click', toggleSortByMenu);
+sortByOptions.forEach(option => {
+    option.addEventListener('click', setSortBy);
+});
 window.addEventListener('scroll', (e) => {
     let inView = isInView(banner);
     if(!inView){
